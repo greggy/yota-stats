b5849d81b6c7545e4d6a3a87759c0f5067eeb07b
Grigory Fateyev <gfborn@gmail.com>
2015-01-31 18:28:36 +0300

    Add RSSI param and timestamp with timezone.

--- yota_stats.py
+++ yota_stats.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
 
+from datetime import datetime
+import time, pytz
 import urllib2
-import time
 import csv
 
 
 URL = 'http://10.0.0.1/status'
 DELAY = 1 # seconds
-PERIOD = 100 # seconds
+PERIOD = 86400 # seconds
 
 
 def run(datawriter, start):
-    now = time.time()
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    now = int(time.time())
     while start + PERIOD > now:
         res = urllib2.urlopen(URL)
         data = res.read().split()
-        SINR = data[19].split('=')[-1]
-        RSRP = data[21].split('=')[-1]
-        ts = str(time.time()).split('.')[0]
-        print('SINR:', SINR, 'RSRP:', RSRP, 'TS', ts)
-        datawriter.writerow([SINR, RSRP, ts])
+        try:
+            SINR = data[19].split('=')[-1]
+            RSSI = data[20].split('=')[-1]
+            RSRP = data[21].split('=')[-1]
+            ts = int(time.mktime(moscow_tz.localize(datetime.now()).timetuple()))
+            print('SINR:', SINR, 'RSSI', RSSI, 'RSRP:', RSRP, 'TS', ts)
+            datawriter.writerow([SINR, RSSI, RSRP, ts])
+        except IndexError:
+            pass
         time.sleep(DELAY)
-        now = time.time()
+        now = int(time.time())
 
 
 def main():
-    start = time.time()
+    start = int(time.time())
     with open('./stats.csv', 'wb') as csvfile:
         datawriter = csv.writer(csvfile)
         run(datawriter, start)
